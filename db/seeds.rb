69878911b1fdea8d8ee139b5421423d53d5ae3e6
github username <github email address>
2017-12-28 19:47:56 -0500

    Test seed data for first bird

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+bird1 = Bird.create!(
+  name: "Sunny",
+  breed: "Cockatiel",
+  description: "Normal Grey, 6 years old, tame, cage included.",
+  photo: File.new("#{Rails.root}/client/public/cats/images/bird_1.jpg"),
+  address: "461 Spring Street, Naugatuck, CT",
+  contact_info: "bird1@gmail.com or 555-555-5555",
+  latitude: 41.511056,
+  longitude: -73.058551
+)
