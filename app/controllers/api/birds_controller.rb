1325f193ffc5e2c3c1823699dd55ddc60483d011
github username <github email address>
2017-12-28 19:38:29 -0500

    finish birds_controller set up and moved api inheritance from application_controller to api_controller

---
+++ app/controllers/api/birds_controller.rb
@@ -0,0 +1,44 @@
+class API::BirdsController < ApiController
+  before_action :set_bird, only: [:show, :update, :destroy]
+
+  def index
+    birds = Bird.all
+    render json: birds, status: 200
+  end
+
+  def show
+    render json: bird, status: 200
+  end
+
+  def create
+    bird = Bird.new(bird_params)
+
+    if bird.save
+      render json: bird
+    else
+      render json: bird.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if bird.update(bird_params)
+      render json: bird
+    else
+      render json: bird.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    bird.destroy
+  end
+
+  private
+  
+    def set_bird
+      bird = Bird.find(params[:id])
+    end
+
+    def bird_params
+      params.require(:bird).permit(:name, :breed, :description, :photo, :address, :contact_info)
+    end
+end
