83f867775186bdef808d0dfe59901e204dd1bec2
github username <github email address>
2017-12-28 19:27:56 -0500

    Install activeAdmin and Devise, add Bird model

---
+++ app/models/bird.rb
@@ -0,0 +1,18 @@
+class Bird < ApplicationRecord
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
+
+  has_attached_file :photo,
+  :storage => :cloudinary,
+  :styles => { :original => "300x300>", :thumb => "100x100>" },
+  :default_url => "/images/adoptable_bird.png",
+  :path => ':id/:style:/:filename'
+
+  do_not_validate_attachment_file_type :photo
+
+
+  def photo_url
+    photo.url(:thumb)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  namespace :api do
+    resources :birds
+    resources :locations
+  end
+
+  # get 'embed_json' => 'application#options', :via => [:options]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
